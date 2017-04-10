4faa55688e058caec9510ce208b6467ffcd29729
Blaž Paradiž <blparadiz@gmail.com>
2017-04-10 12:51:36 +0200

    -remove schema

--- ReadyRoll/dbo/Tables/dbo.Department.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Department] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (50) NULL,
-    [Location] NVARCHAR (50) NULL,
-    [Address]  NVARCHAR (50) NULL,
-    CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- ReadyRoll/dbo/Tables/dbo.Folder.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Folder] (
-    [Id]     INT           IDENTITY (1, 1) NOT NULL,
-    [Name]   NVARCHAR (50) NOT NULL,
-    [Parent] INT           NULL,
-    CONSTRAINT [PK_Folder] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
